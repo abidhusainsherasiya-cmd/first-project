b56c796e0447f2517b61e3644d40b39a1cef0d4d
abidhusain sherasiya <abidhusainsherasiya@gmail.com>
2026-09-16 06:39:52 +0000

    reWrite

--- about.php
+++ about.php
@@ -62,27 +62,27 @@
         <div class="row text-center g-4">
             <div class="col-md-3">
                 <div class="p-3">
-                    <h3 class="fw-bold">100+</h3>
-                     <p class="h-center mb-0">Employee</p>   
+                    <h3 class="fw-bold text-black">100+</h3>
+                     <p class="h-center mb-0 text-black">Employee</p>   
                 </div>
             </div>
             
             <div class="col-md-3">
                 <div class="p-3">
-                    <h3 class="fw-bold">250+</h3>
-                     <p class="h-center mb-0">Project completed</p>   
+                    <h3 class="fw-bold text-black">250+</h3>
+                     <p class="h-center mb-0 text-black">Project completed</p>   
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="p-3">
-                    <h3 class="fw-bold">150+</h3>
-                    <p class="h-center mb-0">Happy Clients</p>
+                    <h3 class="fw-bold text-black">150+</h3>
+                    <p class="h-center mb-0 text-black">Happy Clients</p>
                 </div>
             </div>  
             <div class="col-md-3">
                 <div class="p-3">
-                    <h3 class="fw-bold">10+</h3>
-                    <p class="h-center mb-0">Year of Experience</p>    
+                    <h3 class="fw-bold text-black">10+</h3>
+                    <p class="h-center mb-0 text-black">Year of Experience</p>    
                 </div>
             </div>
         </div>
